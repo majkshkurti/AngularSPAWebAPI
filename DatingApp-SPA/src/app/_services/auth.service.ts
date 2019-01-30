d374f59bd18725bfa84abb0b7296a00fdb254c59
Majk Shkurti <majkshkurti@outlook.com>
2019-01-30 17:48:00 +0100

    Work on Section 11

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import {JwtHelperService} from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class AuthService {
   baseUrl = environment.apiUrl + 'auth/';
   jwtHelper = new JwtHelperService();
   decodedToken: any;
+  currentUser: User;
 constructor(private http: HttpClient) { }
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model)
@@ -19,7 +21,9 @@ constructor(private http: HttpClient) { }
           const user = response;
           if (user) {
             localStorage.setItem('token', user.token);
+            localStorage.setItem('user',JSON.stringify(user.user));
             this.decodedToken = this.jwtHelper.decodeToken(user.token);
+            this.currentUser = user.user;
             console.log(this.decodedToken);
           }
         })
--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
@@ -16,7 +17,9 @@ namespace DatingApp.API.Controllers {
     public class AuthController : ControllerBase {
         private readonly IAuthRepository _repo;
         private readonly IConfiguration _config;
-        public AuthController (IAuthRepository repo, IConfiguration config) {
+        private readonly IMapper _mapper;
+        public AuthController (IAuthRepository repo, IConfiguration config, IMapper mapper) {
+            _mapper = mapper;
             _config = config;
             _repo = repo;
         }
@@ -37,34 +40,35 @@ namespace DatingApp.API.Controllers {
 
         [HttpPost ("login")]
         public async Task<IActionResult> Login (UserForLoginDto userForLoginDto) {
-        
-            var userFromRepo = await _repo.Login (userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            var userFromRepo = await _repo.Login (userForLoginDto.Username.ToLower (), userForLoginDto.Password);
             if (userFromRepo == null)
                 return Unauthorized ();
 
-
             var claims = new [] {
                 new Claim (ClaimTypes.NameIdentifier, userFromRepo.Id.ToString ()),
-                    new Claim (ClaimTypes.Name, userFromRepo.Username)
+                new Claim (ClaimTypes.Name, userFromRepo.Username)
             };
-          
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
-          
-            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
-          
-            var tokenDescriptor = new SecurityTokenDescriptor{
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(1),
+
+            var key = new SymmetricSecurityKey (Encoding.UTF8.GetBytes (_config.GetSection ("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials (key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity (claims),
+                Expires = DateTime.Now.AddDays (1),
                 SigningCredentials = creds
             };
 
-            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenHandler = new JwtSecurityTokenHandler ();
 
-            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.CreateToken (tokenDescriptor);
+            var user = _mapper.Map<UserForListDto>(userFromRepo);
 
-            return Ok(new {
-                token = tokenHandler.WriteToken(token)
-            });
+                return Ok (new {
+                    token = tokenHandler.WriteToken (token),
+                    user
+                });
 
         }
     }

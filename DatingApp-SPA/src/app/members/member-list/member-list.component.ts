14272351c9ae10309ac3309d4809efda4b5bb338
Majk Shkurti <majkshkurti@outlook.com>
2019-02-06 17:05:18 +0100

    Work on Section 14

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -16,9 +16,8 @@ export class MemberListComponent implements OnInit {
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.users = data['users'];
+      this.users = data['users'].result;
     });
   }
-
 
 }
--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Helpers;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -36,9 +37,9 @@ namespace DatingApp.API.Data {
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers () {
-            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
-            return users;
+        public async Task<PagedList<User>> GetUsers (UserParams userParams) {
+            var users =  _context.Users.Include(p => p.Photos);
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public  async Task<bool> SaveAll () {
